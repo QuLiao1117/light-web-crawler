1c8adfeb0c7012a353b62de40c88c40866f4e41d
Liao Qu <1483690505@qq.com>
2020-06-26 00:14:48 +0800

    爬取全国所有省份的top5景点，修缮脚本代码

--- projects/landmarkInfo.py
+++ projects/landmarkInfo.py
@@ -1,10 +1,12 @@
 import json
 from selenium import webdriver
 
-city_name = '北京'
+province_lis = ['河北', '山西', '辽宁', '吉林', '黑龙江', '江苏', '浙江', '安徽', '福建', '江西', '山东', '河南', '湖北', '湖南', '广东',
+                '海南', '四川', '贵州', '云南', '陕西', '甘肃', '青海', '台湾', '西藏', '广西', '内蒙古', '宁夏', '新疆',
+                '北京', '上海', '天津', '重庆', '香港', '澳门']
 
 
-def get_city_top5_landmark_info(city_name):
+def get_place_top5_landmark_info(city_name):
     driver = webdriver.Chrome()
     driver.get("https://www.mafengwo.cn/mdd/")
     dic = {}
@@ -34,10 +36,12 @@ def get_city_top5_landmark_info(city_name):
         dic[place_name[0]]["交通"] = dd[0].text
         dic[place_name[0]]["门票"] = dd[1].text
         dic[place_name[0]]["开放时间"] = dd[2].text
+    driver.close()
     json_str = json.dumps(dic)
     with open(".\\docs\\" + city_name + '.json', 'w') as json_file:
         json_file.write(json_str)
 
 
 if __name__ == "__main__":
-    get_city_top5_landmark_info(city_name)
+    for province in province_lis:
+        get_place_top5_landmark_info(province)
