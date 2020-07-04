4057ec30c34dacc1bc2cefb51fed92585fc354f9
Liao Qu <1483690505@qq.com>
2020-07-04 23:34:05 +0800

    景点信息规范完毕

--- projects/landmark_info_crawler.py
+++ projects/landmark_info_crawler.py
@@ -10,17 +10,22 @@ Args:
 """
 
 import json
+import os
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
+from pathlib import Path
 
-def get_place_top5_landmark_info(location, landmark_number=5):
+
+def get_place_top5_landmark_info(driver, location, save_path, landmark_number=5):
     '''
     给定地点名，查找该地点排名前5的景点，将景点的信息写入json文件
 
     Args:
+        driver: 一个Selenium浏览器对象
         location:地点名
         landmark_number:需要获取的top景点数目
+        save_path: 保存路径
     '''
     driver = webdriver.Chrome()
     driver.get("https://www.mafengwo.cn/mdd/")
@@ -57,15 +62,21 @@ def get_place_top5_landmark_info(location, landmark_number=5):
         dic[place_name[0]]["开放时间"] = dd_content[2].text
     driver.close()
     json_str = json.dumps(dic, ensure_ascii=False)
-    with open("..\\docs\\city_landmark_info\\" + location + '.json', 'w', encoding='utf-8') as json_file:
+    data_folder = Path(save_path)
+    location_path = data_folder / (location + ".json")
+    with open(location_path, 'w', encoding='utf-8') as json_file:
         json_file.write(json_str)
 
 
 if __name__ == "__main__":
-    LOCATIONS = ['河北', '山西', '辽宁', '吉林', '黑龙江', '江苏', '浙江', '安徽', '福建',
+    """ LOCATIONS = ['河北', '山西', '辽宁', '吉林', '黑龙江', '江苏', '浙江', '安徽', '福建',
                  '江西', '山东', '河南', '湖北', '湖南', '广东', '海南', '四川', '贵州',
                  '云南', '陕西', '甘肃', '青海', '台湾', '西藏', '广西', '内蒙古', '宁夏',
-                 '新疆', '北京', '上海', '天津', '重庆', '香港', '澳门']
-    GET_LANDMARK_NUM = 5
+                 '新疆', '北京', '上海', '天津', '重庆', '香港', '澳门'] """
+    LOCATIONS = ['河北']
+    ROOT_DIR = os.path.abspath(os.path.join(os.getcwd(), ".")) + '/docs/city_landmark_info'
+    print(ROOT_DIR)
+    BROWSER_OBJ = webdriver.Chrome()
+    GET_LANDMARK_NUM =  5
     for loc in LOCATIONS:
-        get_place_top5_landmark_info(loc, landmark_number=GET_LANDMARK_NUM)
+        get_place_top5_landmark_info(BROWSER_OBJ, loc, ROOT_DIR, landmark_number=GET_LANDMARK_NUM)
